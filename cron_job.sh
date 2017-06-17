721de8f70e6e46a60a8584fd31640192c03ccbc5
gram <gr@m-gan.sl>
2017-06-17 19:41:18 +0000

    cleaning

--- cron_job.sh
+++ cron_job.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# work
 date >> /home/ubuntu/scimirrorbot/logs/test.log
 
+# authenticate
+eval $(ssh-agent -s)
+ssh-add /home/ubuntu/.ssh/scimirrorbot-git
+
+# push
 cd /home/ubuntu/scimirrorbot/
 git add *
 git commit -m '$date'
