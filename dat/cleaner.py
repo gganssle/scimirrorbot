0ff5de0869c2926229eb776785eeaaf6ce9884cf
gram <gr@m-gan.sl>
2017-06-21 07:07:33 -0500

    cleaning data

--- dat/cleaner.py
+++ dat/cleaner.py
@@ -5,7 +5,13 @@ import re
 with open('20170620.scrape', 'r') as f:
 	inp = f.read()
 
+# remove links
 outp = re.sub(r"http\S+", "", inp)
+# remove the "RT" for retweet
+outp = re.sub(r"RT ", "", outp)
+# replace double + triple \n with singles
+outp = re.sub(r"\n\n", "\n", outp)
+outp = re.sub(r"\n\n\n", "\n", outp)
 
 with open('20170620.clean', 'w') as f:
 	f.write(outp)
